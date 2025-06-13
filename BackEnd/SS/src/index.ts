892abac92fcef05df1d9a2fc5bc9751b2893079b
Akshar <dattarman63gmail.com>
2025-06-13 17:25:18 +0530

    updated changes

--- BackEnd/SS/src/index.ts
+++ BackEnd/SS/src/index.ts
@@ -4,7 +4,12 @@ import userRouter from './routes/user';
 import {cors} from 'hono/cors';
 
 
-const app = new Hono()
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string
+    JWT_SECRET: string
+  }
+}>();
 
 app.use("/*" , cors())
 app.route("/api/v1/user" , userRouter);
