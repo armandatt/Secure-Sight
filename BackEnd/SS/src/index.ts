f491bf436b5b690fb0fd6a22f28ccf10aeee1a44
Akshar <dattarman63gmail.com>
2025-06-13 17:38:54 +0530

    updated cors

--- BackEnd/SS/src/index.ts
+++ BackEnd/SS/src/index.ts
@@ -11,7 +11,11 @@ const app = new Hono<{
   }
 }>();
 
-app.use("/*" , cors())
+app.use("/*" , cors({
+    origin: "*",
+    allowMethods: ['GET', 'POST', 'OPTIONS'],
+    allowHeaders: ['Content-Type', 'Authorization'],
+  }))
 app.route("/api/v1/user" , userRouter);
 app.route("/api/v1/verify" , VerifyRouter);
 
