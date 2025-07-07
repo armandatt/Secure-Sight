15e51d41dc5b265629d998e30310ac3bd9441c6f
Akshar <dattarman63gmail.com>
2025-07-07 16:12:47 +0530

    updated for 404

--- BackEnd/SS/src/routes/user.tsx
+++ BackEnd/SS/src/routes/user.tsx
@@ -3,8 +3,6 @@ import { PrismaClient } from '@prisma/client/edge';
 import { withAccelerate } from "@prisma/extension-accelerate";
 import { decode, sign, verify } from 'hono/jwt';
 import VerifyRouter from "./verify";
-import { constrainedMemory } from "process";
-import { error } from "console";
 
 
 // import { signupInput ,signinInput } from '../../../common/src/index';
